97b8873cd9869259cc83f0de554dde0b1db1e86a
Isaias D Rocha <isaiasdrocha@gmail.com>
2014-07-18 15:19:52 -0300

    Apenas atualização

--- src/br/com/isaiasdrocha/controller/PessoaController.java
+++ src/br/com/isaiasdrocha/controller/PessoaController.java
@@ -2,26 +2,49 @@ package br.com.isaiasdrocha.controller;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.isaiasdrocha.model.Pessoa;
 import br.com.isaiasdrocha.service.PessoaService;
 
 @ManagedBean(name = "pessoaController")
 @ViewScoped
 public class PessoaController  implements Serializable {
 
-	/**
-	 * 
-	 */
+	/*****************************************************************/
+	/*************************** ATRIBUTOS ***************************/
+	/*****************************************************************/
 	private static final long serialVersionUID = 764693509311660129L;
 	
 	@ManagedProperty("#{pessoaRepository}")
 	private PessoaService pessoaService;
-
+	
+	private final Logger log = LoggerFactory.getLogger( PessoaController.class );
+	
+	private Pessoa pessoa;
+	/*****************************************************************/
+	/************************* INICIALIZACAO *************************/
+	/*****************************************************************/
+	@PostConstruct
+	public void initialize() {
+		log.info("Carregar objetos");
+		pessoa = new Pessoa();
+	}
+	
+	
 	public void teste(){
 		pessoaService.teste();
+	}
+	
+	public void imprimeTela(){
+		//FacesContextUtil.addMessage(Severity.WARN, MessageBundleUtil.getMessage("geral_pesquisa_sem_resultado"), null, true);
+		
 	}
 	
 	
@@ -30,6 +53,16 @@ public class PessoaController  implements Serializable {
 	/*********************************************************/
 	public void setPessoaService(PessoaService pessoaService) {
 		this.pessoaService = pessoaService;
+	}
+
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
 	}
 	
 	
